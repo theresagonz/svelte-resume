9138cb09926c21f4c82df5162c806ee1c5bdfe3b
theresamorelli <theresa.l.morelli@gmail.com>
2020-01-29 00:36:38 -0800

    Minor restructure contact section

--- src/components/sections/Contact.svelte
+++ src/components/sections/Contact.svelte
@@ -11,40 +11,40 @@
 <div id="contact-section">
 	{#each details as { phone, email, city, linkedin, web, blog }}
 		{#if email}
-			<div class="item">
+			<div class="contact-item">
 				<i class="fa fa-envelope" />
 				{email}
 			</div>
 		{/if}
 		{#if phone}
-			<div class="item">
+			<div class="contact-item">
 				<i class="fa fa-phone-alt" />
 				{phone}
 			</div>
 		{/if}
 		{#if city}
-			<div class="item">
+			<div class="contact-item">
 				<i class="fa fa-map-marker-alt" />
 				{city}
 			</div>
 		{/if}
+		{#if web}
+		<div class="contact-item">
+			<i class="fa fa-link" />
+			<a href="https://{web}">{web}</a>
+		</div>
+		{/if}
+		{#if blog}
+		<div class="contact-item">
+			<i class="fa fa-pencil-alt" />
+			<a href="https://{blog}">{blog}</a>
+		</div>
+		{/if}
 		{#if linkedin}
-			<div class="item">
+			<div class="contact-item">
 				<i class="fab fa-linkedin-in" />
 				<a href="https://{linkedin}">{linkedin}</a>
 			</div>
 		{/if}
-		{#if web}
-			<div class="item">
-				<i class="fa fa-link" />
-				<a href="https://{web}">{web}</a>
-			</div>
-		{/if}
-		{#if blog}
-			<div class="item">
-				<i class="fa fa-pencil-alt" />
-				<a href="https://{blog}">{blog}</a>
-			</div>
-		{/if}
-	{/each}
+		{/each}
 </div>
