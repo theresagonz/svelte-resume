00ddbff8d8683869991c3b4b9b2c0dc4b84e8d49
theresamorelli <theresa.l.morelli@gmail.com>
2020-03-13 15:03:05 -0700

    Add footer

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,11 @@
 <script>
 	import Header from './components/structural/Header.svelte';
 	import Body from './components/structural/Body.svelte';
+	import Footer from './components/structural/Footer.svelte';
 </script>
 
 <main>
 	<Header />
 	<Body />
+	<Footer />
 </main>
